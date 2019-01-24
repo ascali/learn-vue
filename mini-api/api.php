3253a09dfdf42f9a21c1ebfb70d6a4f621f4da8a
Ascali <djaseem21@gmail.com>
2019-01-24 10:37:43 +0700

    form with example mini api

---
+++ mini-api/api.php
@@ -0,0 +1,13 @@
+<?php
+// untuk mencegah error akibat CORS
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Credentials: true');  
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); 
+
+echo "SIMULASI KIRIM DATA FORM <hr>";
+
+// menampilkan data yang dikirimkan dengan method post
+// print_r($_POST);
+header('Content-type: application/json');
+echo json_encode($_POST, JSON_PRETTY_PRINT);
+print_r($_FILES['cover']);
